7d2dbe8182be009559fed30e1632d5a28573387e
shadowbq <shadowbq@gmail.com>
2014-01-09 11:08:27 -0500

    added 4 msg broadcasts to hello

--- lib/hello.rb
+++ lib/hello.rb
@@ -13,12 +13,19 @@ x = channel.fanout("hello")
 
 queue = channel.queue("hellorb")
 
+msg = "Hello World!  => #{SecureRandom.base64.delete('/+=')[0, 8]}"
 rmsg = "Hello World! Routed => #{SecureRandom.base64.delete('/+=')[0, 8]}"
-msg = "Hello World! => #{SecureRandom.base64.delete('/+=')[0, 8]}"
+emsg = "Hello World! (hello exchange) => #{SecureRandom.base64.delete('/+=')[0, 8]}"
+ermsg = "Hello World! Routed (hello exchange)=> #{SecureRandom.base64.delete('/+=')[0, 8]}"
 
-x.publish(msg)
-x.publish(rmsg, :routing_key => queue.name)
-puts " [x] Route Sent '#{rmsg}'"
+x.publish(emsg)
+x.publish(ermsg, :routing_key => queue.name)
+channel.default_exchange.publish(msg)
+channel.default_exchange.publish(rmsg, :routing_key => queue.name)
+
 puts " [x] Sent '#{msg}'"
+puts " [x] Route Sent '#{rmsg}'"
+puts " [Ex] Sent '#{emsg}'"
+puts " [Ex] Routed Sent '#{ermsg}'"
 
 conn.close
